4450ec74b71d835aa55b48e687ea251b023ed291
Kimala Cochran <kimalajoy@gmail.com>
2020-01-28 15:09:20 -0700

    Completing card test and adding turn and turn test

---
+++ Turn.js
@@ -0,0 +1,34 @@
+class Turn {
+  constructor(guess, card) {
+    this.guess = guess;
+    this.card = card;
+  }
+  returnGuess() {
+    return this.guess;
+  }
+
+  returnCard() {
+    return this.card;
+  }
+
+  evaluateGuess() {
+    return this.guess === this.card.correctAnswer;
+  }
+
+  giveFeedback() {
+    if(this.guess === this.card.correctAnswer) {
+      return 'correct!';
+    } else {
+      return 'incorrect!';
+    } //turnary
+  }
+}
+
+
+
+
+
+
+
+
+module.exports = Turn;
